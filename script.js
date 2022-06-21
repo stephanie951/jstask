ccbe099cc77d38386ef6ddb501c9805f4f7ae9f3
Stephanie Umenwa <101878297+stephanie951@users.noreply.github.com>
2022-06-21 13:29:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let aboutMe = {
+    Name: "Stephanie",
+    Height: 1.73,
+    Country: "Nigeria"
+}
+
+console.log(aboutMe)
+console.log(aboutMe.Name)
+console.log(aboutMe.Height)
+console.log(aboutMe.Country)
